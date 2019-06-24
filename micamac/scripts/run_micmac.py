d094b412cca4cf340cfcab73e3573f435df78543
Loïc Dutrieux <loic.dutrieux@gmail.com>
2019-06-24 11:04:48 +0200

    argparse dash issue

--- micamac/scripts/run_micmac.py
+++ micamac/scripts/run_micmac.py
@@ -31,7 +31,7 @@ STARTFROM_MAPPING = {'exif': 0, # Default
                      'tawny': 11}
 
 def main(img_dir, lon, lat, radius, resolution, ortho, dem, ply,
-         ncores, utm, clean-intermediary, clean-images, startfrom):
+         ncores, utm, clean_intermediary, clean_images, startfrom):
     if not any([ortho, dem, ply]):
         raise ValueError('You must select at least one of --ortho, --dem and --ply')
     startfrom = STARTFROM_MAPPING[startfrom.lower()]
@@ -167,10 +167,10 @@ def main(img_dir, lon, lat, radius, resolution, ortho, dem, ply,
     if ply:
         pass
 
-    if clean-intermediary:
+    if clean_intermediary:
         clean_intermediary()
 
-    if clean-images:
+    if clean_images:
         clean_images()
 
 
